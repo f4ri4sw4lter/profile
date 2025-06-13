f3b4190fdd1e4ef575fef0e7229fbe5fe04d9650
wfarias <ext.wfarias@aper.com>
2025-06-12 22:12:59 -0300

    Rework

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "https://f4ri4sw4lter.github.io/profile"
-})
